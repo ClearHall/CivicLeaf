9d1809c5cad3c18322de7917c83364e76b97afab
PotatoCurry <potatocurry@protonmail.com>
2020-04-12 00:10:57 -0500

    Change login screen to have login and signup

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Civic Leaf',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
