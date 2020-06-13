53323be4aa918dab7072d0c3f24e7f3cc67be1f0
lingfeishengtian <602353hunter>
2020-06-13 17:02:00 -0500

    remove old code and replace with boilerplate

--- lib/generated_plugin_registrant.dart
+++ lib/generated_plugin_registrant.dart
@@ -5,15 +5,18 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
-import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
+// ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FirestoreWeb.registerWith(registry.registrarFor(FirestoreWeb));
   FirebaseAuthWeb.registerWith(registry.registrarFor(FirebaseAuthWeb));
   FirebaseCoreWeb.registerWith(registry.registrarFor(FirebaseCoreWeb));
-  UrlLauncherPlugin.registerWith(registry.registrarFor(UrlLauncherPlugin));
+  SharedPreferencesPlugin.registerWith(registry.registrarFor(SharedPreferencesPlugin));
   registry.registerMessageHandler();
 }
